81e2dbbefd283c899400c09063bc75a2db6f9391
qrapidnow <qrapidnow@gmail.com>
2024-07-09 04:03:01 +0530

    order files

--- app.js
+++ app.js
@@ -7,30 +7,26 @@ const userRouter = require('./routers/user.router');
 const restaurantRouter = require('./routers/restaurant.router');
 const categoryRouter = require('./routers/category.router');
 const itemRouter = require('./routers/item.router');
-const orderRouter = require('./routers/order.router');
-require('dotenv').config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('MongoDB Connected');
-  })
-  .catch(err => {
+}).catch(err => {
     console.log('MongoDB Connection error', err);
-  });
+});
 
-app.use(cors());
+app.use(cors()); // Enable CORS
 app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(userRouter);
 app.use(restaurantRouter);
 app.use(categoryRouter);
 app.use(itemRouter);
-app.use(orderRouter);
+
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: err.message });
+    console.error(err);
+    res.status(500).json({ error: err.message });
 });
 
 module.exports = app;
